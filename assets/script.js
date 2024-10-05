60e269bdb91bf333ef60acb435a5edf711b6cc11
tanyaschmitt2 <tanyamarius@gmail.com>
2024-10-05 12:28:20 -0400

    First commit

---
+++ assets/script.js
@@ -0,0 +1,75 @@
+const slides = [
+    {
+        "image":"slide1.jpg",
+        "tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image":"slide2.jpg",
+        "tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image":"slide3.jpg",
+        "tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image":"slide4.png",
+        "tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+    }
+];
+
+// Constants 
+const arrowbtnleft = document.querySelector(".arrow_left");
+const arrowbtnright = document.querySelector(".arrow_right");
+const dotsContainer = document.querySelector(".dots");
+const lastSlide = slides.length - 1; // last slide checker
+const imageSlide = document.querySelector(".banner-img");
+const baseURL = "./assets/images/slideshow/";
+const tagLine = document.querySelector("#banner p"); 
+
+let slidecourant = 0;
+
+// Add event listeners to the arrows
+arrowbtnleft.addEventListener('click', () => {
+    if (slidecourant === 0) {
+        slidecourant = lastSlide;
+    } else {
+        slidecourant--;
+    }
+    updateSlide(slidecourant);
+});
+
+arrowbtnright.addEventListener('click', () => {
+    if (slidecourant === lastSlide) {
+        slidecourant = 0;
+    } else {
+        slidecourant++;
+    }
+    updateSlide(slidecourant);
+});
+
+// Create a bullet point for each slide and add event listeners
+slides.forEach((slide, index) => {
+    const dot = document.createElement('span');
+    dot.classList.add('dot');
+    if (index === 0) {
+        dot.classList.add('dot-selected'); // Mark the first dot as active
+    }
+    dot.addEventListener('click', () => {
+        updateSlide(index);
+    });
+    dotsContainer.appendChild(dot);
+});
+
+// Function to update the slide
+function updateSlide(index) {
+    imageSlide.src = `${baseURL}${slides[index].image}`;
+    tagLine.innerHTML = slides[index].tagLine;
+
+    // Update active dot
+    document.querySelector('.dot.dot-selected').classList.remove('dot-selected');
+    dotsContainer.children[index].classList.add('dot-selected');
+}
+
+// Initialize the first slide
+updateSlide(slidecourant);
+
